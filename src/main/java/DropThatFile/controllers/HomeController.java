6456d012efbfe12a4c76193f3ea02c2c6f887189
OlivierLefebvre96 <olivier.lefebvre@akeonet.com>
2017-05-15 23:00:08 +0200

    Refonte de l'IHM + ajout d'un lien hypertexte au bouton approprié

--- src/main/java/DropThatFile/controllers/HomeController.java
+++ src/main/java/DropThatFile/controllers/HomeController.java
@@ -1,10 +1,17 @@
 package DropThatFile.controllers;
 
-import DropThatFile.engines.windowsManager.WindowsHandler;
 import DropThatFile.engines.windowsManager.userForms.HomeForm;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.AnchorPane;
-
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,9 +19,6 @@ import java.util.ResourceBundle;
  * Created by Travail on 02/05/2017.
  */
 public class HomeController extends AnchorPane implements Initializable {
-
-    private WindowsHandler windowsHandler;
-
     private HomeForm application;
 
     public void setApp(HomeForm application){
@@ -24,5 +28,15 @@ public class HomeController extends AnchorPane implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    @FXML
+    public void openLink() throws URISyntaxException, IOException {
+        Desktop desktop = Desktop.getDesktop();
+        if (desktop.isSupported(Desktop.Action.BROWSE)) {
+            desktop.browse(new URI("www.google.com"));
+        } else {
+            // ...
+        }
     }
 }
